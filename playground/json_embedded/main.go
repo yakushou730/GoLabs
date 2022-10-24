a055f681ca888f9a4a4e1ef815bae236b6de4aae
Shou Tseng <yakushou730@gmail.com>
2022-10-24 21:54:58 +0800

    feat(playground): json embedded

---
+++ playground/json_embedded/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Layer1 struct {
+	Layer2
+	Layer4 `json:"layer_4"`
+}
+
+type Layer2 struct {
+	Layer3
+}
+
+type Layer3 struct {
+	Name string `json:"name"`
+}
+
+type Layer4 struct {
+	Age int `json:"age"`
+}
+
+func main() {
+	l := Layer1{
+		Layer2{
+			Layer3{
+				Name: "shou",
+			},
+		},
+		Layer4{Age: 18},
+	}
+	data, _ := json.MarshalIndent(l, "", "  ")
+	fmt.Printf("%v\n", string(data))
+}
