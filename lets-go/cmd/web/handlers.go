af963318801a197283528065a468b24cc361fb43
Shou Tseng <yakushou730@gmail.com>
2022-10-30 16:37:59 +0800

    feat(lets-go): executing sql statements

--- lets-go/cmd/web/handlers.go
+++ lets-go/cmd/web/handlers.go
@@ -48,5 +48,15 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("create a new snippet..."))
+	title := "Shou"
+	content := "Shou is the best"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
