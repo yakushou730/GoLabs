5823c63c3c82cf33a3c20c2bd063bc84d611b298
Shou Tseng <yakushou730@gmail.com>
2022-11-02 20:53:50 +0800

    feat(lets-go): routes setup

--- lets-go/cmd/web/handlers.go
+++ lets-go/cmd/web/handlers.go
@@ -96,3 +96,23 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 	app.render(w, http.StatusOK, "create.tmpl", data)
 }
+
+func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display a HTML form for signing up a new user...")
+}
+
+func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create a new user...")
+}
+
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display a HTML form for logging in a user...")
+}
+
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Authenticate and login the user...")
+}
+
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Logout the user...")
+}
