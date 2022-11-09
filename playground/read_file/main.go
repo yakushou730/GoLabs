1961439102b2aef94851b6c55df420d6a44c9658
Shou Tseng <yakushou730@gmail.com>
2022-11-09 22:27:53 +0800

    Auto stash before rebase of "book-100-mistake"

--- playground/read_file/main.go
+++ playground/read_file/main.go
@@ -8,4 +8,13 @@ import (
 func main() {
 	data, _ := os.ReadFile("test.txt")
 	fmt.Printf("%v\n", string(data))
+
+	//
+	file, err := os.Open("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	//scanner := bufio.NewScanner(file)
 }
