0c049ed10a29a29efdfa2c0e9756eb4f91f99595
Shou Tseng <yakushou730@gmail.com>
2022-11-10 10:45:39 +0800

    feat(lets-go): testing HTTP handlers and middleware

---
+++ lets-go/cmd/web/handlers_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"golabs/lets-go/internal/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	rr := httptest.NewRecorder()
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ping(rr, r)
+
+	rs := rr.Result()
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+	defer rs.Body.Close()
+
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
+}
