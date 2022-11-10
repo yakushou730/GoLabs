5eac1110b002d3419aec7e35a38125f18fda3297
Shou Tseng <yakushou730@gmail.com>
2022-11-10 17:17:31 +0800

    feat(lets-go): add account page

--- lets-go/internal/models/mocks/users.go
+++ lets-go/internal/models/mocks/users.go
@@ -1,6 +1,9 @@
 package mocks
 
-import "golabs/lets-go/internal/models"
+import (
+	"golabs/lets-go/internal/models"
+	"time"
+)
 
 type UserModel struct{}
 
@@ -28,4 +31,19 @@ func (m *UserModel) Exists(id int) (bool, error) {
 	default:
 		return false, nil
 	}
+}
+
+func (m *UserModel) Get(id int) (*models.User, error) {
+	if id == 1 {
+		u := &models.User{
+			ID:      1,
+			Name:    "YakuShou",
+			Email:   "yakushou@gmail.com",
+			Created: time.Now(),
+		}
+
+		return u, nil
+	}
+
+	return nil, models.ErrNoRecord
 }
