52ca67f3fcef202023c52132d58e7f8fe8ac2cf3
Shou Tseng <yakushou730@gmail.com>
2022-11-10 16:28:26 +0800

    feat(lets-go): add a debug mode

--- lets-go/cmd/web/main.go
+++ lets-go/cmd/web/main.go
@@ -18,6 +18,7 @@ import (
 )
 
 type application struct {
+	debug          bool
 	errorLog       *log.Logger
 	infoLog        *log.Logger
 	snippets       models.SnippetModelInterface
@@ -30,6 +31,8 @@ type application struct {
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	dsn := flag.String("dsn", "root:secret@tcp(localhost:30000)/snippetbox?parseTime=true", "MySQL data source name")
+	debug := flag.Bool("debug", false, "Enable debug mode")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -54,6 +57,7 @@ func main() {
 	sessionManager.Cookie.Secure = true
 
 	app := &application{
+		debug:    *debug,
 		errorLog: errorLog,
 		infoLog:  infoLog,
 		snippets: &models.SnippetModel{
