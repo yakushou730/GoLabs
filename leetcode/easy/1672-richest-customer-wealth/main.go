f5a9b409eda58f3650f59678d4b5f2e6326fbddb
Shou Tseng <yakushou730@gmail.com>
2022-11-21 09:33:39 +0800

    feat(leetcode): 1672. richest customer wealth

--- leetcode/easy/1672-richest-customer-wealth/main.go
+++ leetcode/easy/1672-richest-customer-wealth/main.go
@@ -1,15 +1,13 @@
 package main
 
+// https://leetcode.com/problems/richest-customer-wealth/
 func maximumWealth(accounts [][]int) int {
-	m := len(accounts)
 	max := 0
 
-	for i := 0; i < m; i++ {
-		n := len(accounts[i])
+	for _, account := range accounts {
 		tmp := 0
-		for j := 0; j < n; j++ {
-			//fmt.Printf("accounts[%d][%d] = %d\n", i, j, accounts[i][j])
-			tmp += accounts[i][j]
+		for _, bank := range account {
+			tmp += bank
 		}
 		if tmp > max {
 			max = tmp
@@ -18,3 +16,22 @@ func maximumWealth(accounts [][]int) int {
 
 	return max
 }
+
+//func maximumWealth(accounts [][]int) int {
+//	m := len(accounts)
+//	max := 0
+//
+//	for i := 0; i < m; i++ {
+//		n := len(accounts[i])
+//		tmp := 0
+//		for j := 0; j < n; j++ {
+//			//fmt.Printf("accounts[%d][%d] = %d\n", i, j, accounts[i][j])
+//			tmp += accounts[i][j]
+//		}
+//		if tmp > max {
+//			max = tmp
+//		}
+//	}
+//
+//	return max
+//}
