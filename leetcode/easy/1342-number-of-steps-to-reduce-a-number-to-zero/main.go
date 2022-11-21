1b7fa33cb402a55cc3f56e300caf3151390809f7
Shou Tseng <yakushou730@gmail.com>
2022-11-21 10:08:47 +0800

    feat(leetcode): 1342. Number of Steps to Reduce a Number to Zero

--- leetcode/easy/1342-number-of-steps-to-reduce-a-number-to-zero/main.go
+++ leetcode/easy/1342-number-of-steps-to-reduce-a-number-to-zero/main.go
@@ -2,22 +2,17 @@ package main
 
 // https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
 func numberOfSteps(num int) int {
-	times := 0
+	steps := 0
 
-	for {
-		if num == 0 {
-			break
-		}
-
-		switch num % 2 {
-		case 0:
+	for num > 0 {
+		if num%2 == 0 {
 			num /= 2
-		default:
-			num -= 1
+		} else {
+			num--
 		}
 
-		times++
+		steps++
 	}
 
-	return times
+	return steps
 }
