372a5a25e172f1a0d48f0f7a4b9af21965293cdb
Shou Tseng <yakushou730@gmail.com>
2022-11-24 21:24:06 +0800

    feat: 1.5 start of a REPL

---
+++ interpreter/repl/repl.go
@@ -0,0 +1,33 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"golabs/interpreter/lexer"
+	"golabs/interpreter/token"
+	"io"
+)
+
+const (
+	PROMPT = ">> "
+)
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+
+}
