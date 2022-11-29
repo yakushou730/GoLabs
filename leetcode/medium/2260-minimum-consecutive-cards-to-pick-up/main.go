370d458b0edc300a84df23ae04856999804c390c
Shou Tseng <yakushou730@gmail.com>
2022-11-29 16:49:51 +0800

    feat(leetcode): 2260. Minimum Consecutive Cards to Pick Up

--- leetcode/medium/2260-minimum-consecutive-cards-to-pick-up/main.go
+++ leetcode/medium/2260-minimum-consecutive-cards-to-pick-up/main.go
@@ -2,18 +2,17 @@ package main
 
 // https://leetcode.com/problems/minimum-consecutive-cards-to-pick-up/
 func minimumCardPickup(cards []int) int {
-	cardsMap := make(map[int][]int, 0)
+	cardsMap := make(map[int]int, 0)
 	ans := 100_00_00
+	diff := 0
 	for i, card := range cards {
-		cardsMap[card] = append(cardsMap[card], i)
-		if len(cardsMap[card]) < 2 {
-			continue
-		} else {
-			diff := cardsMap[card][len(cardsMap[card])-1] - cardsMap[card][len(cardsMap[card])-2] + 1
+		if prev, ok := cardsMap[card]; ok {
+			diff = i - prev + 1
 			if diff < ans {
 				ans = diff
 			}
 		}
+		cardsMap[card] = i
 	}
 
 	if ans == 100_00_00 {
@@ -22,3 +21,25 @@ func minimumCardPickup(cards []int) int {
 		return ans
 	}
 }
+
+//func minimumCardPickup(cards []int) int {
+//	cardsMap := make(map[int][]int, 0)
+//	ans := 100_00_00
+//	for i, card := range cards {
+//		cardsMap[card] = append(cardsMap[card], i)
+//		if len(cardsMap[card]) < 2 {
+//			continue
+//		} else {
+//			diff := cardsMap[card][len(cardsMap[card])-1] - cardsMap[card][len(cardsMap[card])-2] + 1
+//			if diff < ans {
+//				ans = diff
+//			}
+//		}
+//	}
+//
+//	if ans == 100_00_00 {
+//		return -1
+//	} else {
+//		return ans
+//	}
+//}
