9686e760fb69eea19551ccd6bbb52410b70ab1d5
Shou Tseng <yakushou730@gmail.com>
2022-12-02 10:12:58 +0800

    feat(leetcode): 20. Valid Parentheses

--- leetcode/easy/20-valid-parentheses/main.go
+++ leetcode/easy/20-valid-parentheses/main.go
@@ -17,22 +17,58 @@ func isValid(s string) bool {
 			if stack[len(stack)-1] == '(' {
 				stack = stack[:len(stack)-1]
 			} else {
-				stack = append(stack, ch)
+				return false
 			}
 		case ']':
 			if stack[len(stack)-1] == '[' {
 				stack = stack[:len(stack)-1]
 			} else {
-				stack = append(stack, ch)
+				return false
 			}
 		case '}':
 			if stack[len(stack)-1] == '{' {
 				stack = stack[:len(stack)-1]
 			} else {
-				stack = append(stack, ch)
+				return false
 			}
 		}
 	}
 
 	return len(stack) == 0
 }
+
+//func isValid(s string) bool {
+//	stack := make([]int32, 0)
+//
+//	for _, ch := range s {
+//		if len(stack) == 0 {
+//			stack = append(stack, ch)
+//			continue
+//		}
+//
+//		switch ch {
+//		case '(', '[', '{':
+//			stack = append(stack, ch)
+//		case ')':
+//			if stack[len(stack)-1] == '(' {
+//				stack = stack[:len(stack)-1]
+//			} else {
+//				stack = append(stack, ch)
+//			}
+//		case ']':
+//			if stack[len(stack)-1] == '[' {
+//				stack = stack[:len(stack)-1]
+//			} else {
+//				stack = append(stack, ch)
+//			}
+//		case '}':
+//			if stack[len(stack)-1] == '{' {
+//				stack = stack[:len(stack)-1]
+//			} else {
+//				stack = append(stack, ch)
+//			}
+//		}
+//	}
+//
+//	return len(stack) == 0
+//}
