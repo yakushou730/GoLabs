452ebe061383291660118bf36e979d21e6e665c7
Shou Tseng <yakushou730@gmail.com>
2022-12-15 12:26:56 +0800

    feat(leetcode): 206. Reverse Linked List

---
+++ leetcode/easy/206-reverse-linked-list/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/reverse-linked-list/
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	var ans, prev *ListNode
+	for head != nil {
+		ans = &ListNode{
+			Val:  head.Val,
+			Next: prev,
+		}
+
+		prev = ans
+		head = head.Next
+	}
+
+	return ans
+}
+
+func main() {
+	head1 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val:  5,
+						Next: nil,
+					},
+				},
+			},
+		},
+	}
+
+	ans1 := reverseList(head1)
+
+	for ans1 != nil {
+		fmt.Println(ans1.Val)
+		ans1 = ans1.Next
+	}
+
+}
