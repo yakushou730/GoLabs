0ad041857b015946970004f5e8a4c96b4f97b504
Shou Tseng <yakushou730@gmail.com>
2022-12-28 18:45:25 +0800

    feat(ddd): first draft

---
+++ packt/ddd-go/chapter2/main.go
@@ -0,0 +1,50 @@
+package chapter2
+
+import "context"
+
+type UserType = int
+type SubscriptionType = int
+
+const (
+	unknownUserType UserType = iota
+	lead
+	customer
+	churned
+	lostLead
+)
+
+const (
+	unknownSubscriptionType SubscriptionType = iota
+	basic
+	premium
+	exclusive
+)
+
+type UserAddRequest struct {
+	UserType       UserType
+	Email          string
+	SubType        SubscriptionType
+	PaymentDetails PaymentDetails
+}
+
+type UserModifyRequest struct {
+	ID             string
+	UserType       UserType
+	Email          string
+	SubType        SubscriptionType
+	PaymentDetails PaymentDetails
+}
+
+type User struct {
+	ID             string
+	PaymentDetails PaymentDetails
+}
+
+type PaymentDetails struct {
+	stripeTokenID string
+}
+
+type UserManager interface {
+	AddUser(ctx context.Context, request UserAddRequest) (User, error)
+	ModifyUser(ctx context.Context, request UserModifyRequest) (User, error)
+}
